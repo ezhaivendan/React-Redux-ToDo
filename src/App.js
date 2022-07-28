bd999eedddc26efe185c176e055c8bf27649083d
ezhaivendan <ezhaivendan@gmail.com>
2022-07-28 21:42:06 +0530

    App files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ToDoList from './Components/ToDoList';
+import AddItem from './AddItem';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          <AddItem />
+          <ToDoList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
